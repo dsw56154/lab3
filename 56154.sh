f0ece399d81ecb4da688c8b79af7e0d549a6d8b6
Jakub <dsw56154@student.dsw.edu.pl>
2025-04-07 15:31:44 +0200

    Add files via upload
    description

---
+++ 56154.sh
@@ -0,0 +1,151 @@
+$ git commit 
+
+$ git commit
+
+$ git branch bugFix
+
+$ git checkout bugFix
+
+$ git branch bugFix
+
+$ git checkout bugFix
+
+$ git commit
+
+$ git checkout main
+
+$ git commit
+
+$ git merge bugFix
+
+$ git branch bugFix
+
+$ git checkout bugFix
+
+$ git commit
+
+$ git checkout main
+
+$ git commit
+
+$ git checkout bugFix
+
+$ git rebase main
+
+$ git checkout c4
+
+$ git checkout bugFix^
+
+$ git checkout C6
+
+$ git branch -f main
+
+$ git checkout C0
+
+$ git branch -f bugFix
+
+$ git checkout C1
+
+$ git reset c1
+
+$ git checkout pushed
+
+$ git revert c2
+
+$ git cherry-pick c3 c4 c7
+
+$ git rebase -i overHere
+
+$ git rebase c4' main
+
+$ git checkout c1 
+
+$ git cherry-pick c4
+
+$ git rebase c4' main
+
+$ git rebase -i HEAD~2
+
+$ git checkout C2
+
+$ git rebase -i c2
+
+$ git checkout caption
+
+$ git rebase -i c1
+
+git rebase c3'' main
+
+$ git checkout C1
+
+$ git cherry-pick c2
+
+$ git checkout main
+
+$ git cherry-pick c2 c3
+
+$ git tag v1 c2
+
+$ git tag v0 c1
+
+$ git checkout c2
+
+$ git describe
+
+$ git commit
+
+$ reset
+
+$ git checkout C3
+
+$ git rebase c2
+
+$ git checkout C4
+
+$ git rebase c3'
+
+$ git checkout C5
+
+$ git rebase c4'
+
+$ git checkout C6
+
+$ git rebase c5'
+
+$ git checkout C7
+
+$ git rebase c6'
+
+$ git branch -f another
+
+$ git checkout C6'
+
+$ git branch -f side
+
+$ git checkout C3'
+
+$ git branch -f bugFix
+
+$ git checkout C7'
+
+$ git branch -f main
+
+$ git branch bugWork c2
+
+$ git checkout C1
+
+$ git cherry-pick c4 c3 c2
+
+$ git checkout C1
+
+$ git cherry-pick c5 c4 c3 c2
+
+$ git rebase one c2'
+
+$ git rebase c2' one
+
+$ git rebase c2'' two
+
+$ git rebase c2 three
+
+$ git clone
